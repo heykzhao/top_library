59c62ffb70dc1135a6f03e71aa581d7339e21337
Kevin Zhao <kvnzhao@outlook.com>
2021-10-27 21:09:42 -0400

    Create function to add books to the library

--- app.js
+++ app.js
@@ -1,4 +1,12 @@
-let myLibrary = [];
+// Random books to fill array with
+let book1 = new Book('Dune', 'Frank Herbert', 'Not yet', 'N/A', 'Have not read yet');
+let book2 = new Book('Dune2', 'Frank Herbert', 'Not yet', 'N/A', 'Have not read yet');
+let book3 = new Book('Dune', 'Frank Herbert2', 'Not yet', 'N/A', 'Have not read yet');
+let book4 = new Book('Cool Guy', 'Kevin Zhao', 'Read', '5', 'Greatest book I\'ve ever read');
+
+
+// let myLibrary = [];
+let myLibrary = [book1, book2, book3, book4];
 
 function Book(title, author, read, rating, comment) {
     this.title = title;
@@ -8,10 +16,11 @@ function Book(title, author, read, rating, comment) {
     this.comment = comment; 
 };
 
+
 function addBooktoLibrary(book) {
-    if (book.indexOf() == -1) {
-        myLibrary.push(book);
+    if ((myLibrary.some(e => (e.title == book.title) && (e.author == book.author)))) {
+        alert('It looks like this book is already in your library!');
     } else {
-        alert('You\'ve already added this book!');
+        myLibrary.push(book);
     }
 };
