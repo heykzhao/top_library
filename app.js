19d0e3ed7437e6db6224559eeba2cf6822df28ef
Kevin Zhao <kvnzhao@outlook.com>
2021-10-30 19:54:04 -0400

    Create new book button and modal UI

--- app.js
+++ app.js
@@ -1,12 +1,5 @@
-// Random books to fill array with
-let book1 = new Book('Dune', 'Frank Herbert', 'Not yet', 'N/A', 'Have not read yet');
-let book2 = new Book('Dune2', 'Frank Herbert', 'Not yet', 'N/A', 'Have not read yet');
-let book3 = new Book('Dune', 'Frank Herbert2', 'Not yet', 'N/A', 'Have not read yet');
-let book4 = new Book('Cool Guy', 'Kevin Zhao', 'Read', '5', 'Greatest book I\'ve ever read');
-
-
 // let myLibrary = [];
-let myLibrary = [book1, book2, book3, book4];
+let myLibrary = [];
 
 function Book(title, author, read, rating, comment) {
     this.title = title;
@@ -24,6 +17,62 @@ function addBooktoLibrary(book) {
     }
 };
 
+
+
+// Display Library based on what's in myLibrary object
+const libraryDiv = document.querySelector('.library');
 function displayLibrary() {
-    
+    for (let i = 0; i < myLibrary.length; i++) {
+        let div = document.createElement('div');
+        div.setAttribute('class', 'book-card');
+        div.innerHTML = `
+        <div class="book">
+            <div class="read-not-read ${myLibrary[i].read}">✓</div>
+            <p class='book-title'>${myLibrary[i].title}</p>
+            <p class='book-author'>${myLibrary[i].author}</p>
+        </div>
+        <div class="bottom-of-card">
+            <div class="extra-options noselect">• • •</div>
+        </div>
+        `;
+        libraryDiv.appendChild(div);
+    }
+}
+displayLibrary();
+
+// Display Star Ratings option whenever a book is read
+const bottomOfCardDiv = document.querySelectorAll('.bottom-of-card');
+function displayStarRating() {
+    for (let i = 0; i < bottomOfCardDiv.length; i++) {
+        let div = document.createElement('div');
+        div.setAttribute('class', 'ratings noselect');
+        if (myLibrary[i].read === "read") {
+            div.innerHTML = `
+            <i class="rating-star inactive-star noselect">★</i>
+            <i class="rating-star inactive-star noselect">★</i>
+            <i class="rating-star inactive-star noselect">★</i>
+            <i class="rating-star inactive-star noselect">★</i>
+            <i class="rating-star inactive-star noselect">★</i>
+            `;
+        bottomOfCardDiv[i].insertBefore(div, bottomOfCardDiv[i].firstChild);
+        }
+    };
+};
+displayStarRating();
+
+// Open and closing a new book modal
+const newBookModal = document.querySelector('.new-book-modal');
+const newBookButton = document.querySelector('.new-book-button');
+const closeButton = document.querySelector('.modal-close-button');
+
+newBookButton.onclick = function() {
+    newBookModal.style.display = "block";
+}
+closeButton.onclick = function() {
+    newBookModal.style.display = "none";
+}
+window.onclick = function(event) {
+    if (event.target == newBookModal) {
+        newBookModal.style.display = "none";
+    }
 }
