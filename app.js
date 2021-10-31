f45199467d50eea9e8d18398ac4e845bd86d9d5e
Kevin Zhao <kvnzhao@outlook.com>
2021-10-31 13:33:53 -0400

    Allow ability to rate books that are read

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
-let test = new Book('test','test',true,null,null);
-let test2 = new Book('tesst','test',false,null,null);
+let sampleBook1 = new Book('Bad Blood', 'John Carreyrou', true, '4');
+let sampleBook2 = new Book('An Ugly Truth', 'Sheera Frankel, Cecilia Kang', true, '5');
+let sampleBook3 = new Book('The Every', 'Dave Eggers', false, '');
+
 
 // let myLibrary = [];
-let myLibrary = [test,test2];
+let myLibrary = [sampleBook1, sampleBook2, sampleBook3];
 let readNotReadButton = document.querySelectorAll('.read-not-read');
+let deleteButton = document.querySelectorAll('.extra-options');
+let starButton = document.querySelectorAll('.rating-star');
 
 function Book(title, author, read, rating, comment) {
     this.title = title;
@@ -37,6 +41,7 @@ function addBookFromSubmitButton(e) {
             newBookModal.style.display = "none";
         }
     }
+    starRatingUI();
 }
 
 // Display Library based on what's in myLibrary object
@@ -53,24 +58,22 @@ function displayLibrary() {
             <p class='book-author'>${myLibrary[i].author}</p>
         </div>
         <div class="bottom-of-card">
-            <div class="extra-options noselect">• • •</div>
+            <div class="extra-options noselect">⛔️</div>
         </div>
         `;
         libraryDiv.appendChild(div);
     }
+    deleteButton = document.querySelectorAll('.extra-options');
+    deleteButton.forEach((btn) => {
+        btn.addEventListener('click', deleteBook);
+    })
     readNotReadButton = document.querySelectorAll('.read-not-read');
     readNotReadButton.forEach((btn) => {
-        btn.addEventListener('click',readNotRead);
+        btn.addEventListener('click', readNotRead);
     })
+    starRatingUI();
 }
 displayLibrary();
-
-function removeAllChildNodes(parent) {
-    while (parent.firstChild) {
-        parent.removeChild(parent.firstChild);
-    }
-}
-
 // Display Star Ratings option whenever a book is read
 let bottomOfCardDiv = document.querySelectorAll('.bottom-of-card');
 function displayStarRating() {
@@ -80,11 +83,11 @@ function displayStarRating() {
         let childNodes = bottomOfCardDiv[i].children;
         div.setAttribute('class', 'ratings noselect');
         div.innerHTML = `
-            <i class="rating-star inactive-star noselect">★</i>
-            <i class="rating-star inactive-star noselect">★</i>
-            <i class="rating-star inactive-star noselect">★</i>
-            <i class="rating-star inactive-star noselect">★</i>
-            <i class="rating-star inactive-star noselect">★</i>
+            <i class="rating-star inactive-star noselect 1-star">★</i>
+            <i class="rating-star inactive-star noselect 2-star">★</i>
+            <i class="rating-star inactive-star noselect 3-star">★</i>
+            <i class="rating-star inactive-star noselect 4-star">★</i>
+            <i class="rating-star inactive-star noselect 5-star">★</i>
             `;
 
         if (myLibrary[i].read === true && childNodes[0].classList.contains('ratings') === false) {
@@ -94,6 +97,11 @@ function displayStarRating() {
             bottomOfCardDiv[i].removeChild(childNodes[0]);
         }
     };
+    starButton = document.querySelectorAll('.rating-star');
+    starButton.forEach((btn) => {
+        btn.addEventListener('click', rateBook);
+    })
+    starRatingUI();
 };
 displayStarRating();
 
@@ -119,10 +127,73 @@ function readNotRead(e) {
     if (e.target.classList.contains('true')) {
         e.target.classList.remove('true');
         myLibrary[e.target.parentNode.id].read = false;
+        myLibrary[e.target.parentNode.id].rating = '';
         displayStarRating();
+        starRatingUI();
     } else {
         e.target.classList.add('true');
         myLibrary[e.target.parentNode.id].read = true;
+        myLibrary[e.target.parentNode.id].rating = '';
         displayStarRating();
+        starRatingUI();
+    }
+}
+
+// Delete button
+function deleteBook(e) {
+    let deleteBookIndex = e.target.parentNode.parentNode.firstElementChild.id;
+    myLibrary.splice(deleteBookIndex, 1);
+    displayLibrary();
+    displayStarRating();
+    starRatingUI();
+}
+
+// Star Rating
+function rateBook(e) {
+    let rateBookIndex = e.target.parentNode.parentNode.parentNode.firstElementChild.id;
+    let starsGiven;
+    if (e.target.classList.contains("1-star")) {
+        starsGiven = 1;
+    } else if (e.target.classList.contains("2-star")) {
+        starsGiven = 2;
+    } else if (e.target.classList.contains("3-star")) {
+        starsGiven = 3;
+    } else if (e.target.classList.contains("4-star")) {
+        starsGiven = 4;
+    } else if (e.target.classList.contains("5-star")) {
+        starsGiven = 5;
+    }
+    myLibrary[rateBookIndex].rating = starsGiven;
+    starRatingUI();
+}
+
+function starRatingUI() {
+    let bookRatings = [];
+    for (let i = 0; i < myLibrary.length; i++) {
+        bookRatings.push(myLibrary[i].rating);
+    }
+    let bottomOfCardDiv = document.querySelectorAll('.bottom-of-card');
+    for (let i = 0; i < bottomOfCardDiv.length; i++) {
+        if (bottomOfCardDiv[i].children[0].classList.contains('ratings')) {
+            let specificBookRating = bookRatings[i];
+            let specificBookRatingDiv = bottomOfCardDiv[i].children[0].children;
+            for (let j = 0; j < specificBookRatingDiv.length; j++) {
+                if (specificBookRatingDiv[j].classList.contains('active-star')) {
+                    specificBookRatingDiv[j].classList.remove('active-star');
+                    specificBookRatingDiv[j].classList.add('inactive-star');
+                }
+                for (let k = 0; k < specificBookRating; k++) {
+                    specificBookRatingDiv[k].classList.remove('inactive-star');
+                    specificBookRatingDiv[k].classList.add('active-star');
+                }
+            }
+        }
+    }
+}
+starRatingUI();
+
+function removeAllChildNodes(parent) {
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
     }
 }
